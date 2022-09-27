b0d6d4110d7771f89c32b87d474f5f711c263b86
jsakamotoIGJP <jsakamoto@infragistics.com>
2022-09-27 17:39:10 +0900

    サンプルとして不要なファイルとコードを削除して清掃

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
+using IgbDockManagerDemo;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using IgbDockManagerDemo;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
