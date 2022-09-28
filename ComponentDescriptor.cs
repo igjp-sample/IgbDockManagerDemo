249d520258acc43f1ed46913c9403eab9fe77ae2
jsakamotoIGJP <jsakamoto@infragistics.com>
2022-09-28 16:36:48 +0900

    表示するコンポーネントを示すデータ構造を実装

---
+++ ComponentDescriptor.cs
@@ -0,0 +1,16 @@
+﻿namespace IgbDockManagerDemo;
+
+/// <summary>
+/// "画面上に表示されているコンポーネント" の 1 つ 1 つを表すレコードクラス
+/// </summary>
+/// <param name="Id">表示している個々のコンポーネントインスタンスを一意に識別する Id 文字列</param>
+/// <param name="ComponentName">表示するコンポーネントの名前</param>
+/// <param name="Parameters">表示するコンポーネントに引き渡すパラメータ</param>
+public record ComponentDescriptor
+(
+    string Id,
+    string ComponentName,
+    Dictionary<string, object?> Parameters
+);
+
+
