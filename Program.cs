ce8fd5fb50679cd2c1056819759fcc781e12562d
jsakamotoIGJP <jsakamoto@infragistics.com>
2022-09-29 14:09:27 +0900

    ドックマネージャーとやりとりするためのヘルパー機能を実装

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Blazored.LocalStorage;
 using IgbDockManagerDemo;
+using IgbDockManagerDemo.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -10,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddIgniteUIBlazor();
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<DockManagerHelper>();
 
 await builder.Build().RunAsync();
